04f244e28d5bb9429e32f647421ca9ae803a0422
Bench <hocsonbenedict@yahoo.com>
2024-12-26 04:09:24 +0800

    added

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,7 +29,7 @@ def login_page():
         st.markdown("<h1 style='text-align: center;'>Login</h1>", unsafe_allow_html=True)
         col1, col2, col3 = st.columns([1,2,1])
         with col2:
-            username = st.text_input("Username")
+            username = st.text_input("Username", autocomplete="off")
             password = st.text_input("Password", type="password", autocomplete="off")
             
             if st.button("Login"):
